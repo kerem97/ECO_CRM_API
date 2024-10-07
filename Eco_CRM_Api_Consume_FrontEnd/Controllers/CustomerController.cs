329366f84e822bf49ba51a4e6036b51fce844bf2
Kerem DURAK <kerem.durak@yahoo.com>
2024-10-07 11:02:59 +0300

    up

--- Eco_CRM_Api_Consume_FrontEnd/Controllers/CustomerController.cs
+++ Eco_CRM_Api_Consume_FrontEnd/Controllers/CustomerController.cs
@@ -47,6 +47,39 @@ namespace Eco_CRM_Api_Consume_FrontEnd.Controllers
             ViewBag.CurrentPage = pageNumber;
             return View(customerList);
         }
+        public async Task<IActionResult> Index2(int pageNumber = 1)
+        {
+            var fullName = HttpContext.Session.GetString("FullName");
+            ViewBag.FullName = fullName;
+
+            var client = _httpClientFactory.CreateClient();
+            client.DefaultRequestHeaders.Add("Authorization", $"Bearer {HttpContext.Session.GetString("Token")}");
+            ViewBag.Token = HttpContext.Session.GetString("Token");
+
+            var response = await client.GetAsync($"https://localhost:44309/api/Customers/paged-customers?pageNumber={pageNumber}&pageSize=8");
+
+            if (!response.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Login", "Account");
+            }
+
+            var customerList = new List<DisplayCustomerResponse>();
+
+            if (response.IsSuccessStatusCode)
+            {
+                var responseContent = await response.Content.ReadAsStringAsync();
+                customerList = JsonConvert.DeserializeObject<List<DisplayCustomerResponse>>(responseContent);
+
+                var totalRecordsHeader = response.Headers.GetValues("X-Total-Count").FirstOrDefault();
+                if (int.TryParse(totalRecordsHeader, out int totalRecords))
+                {
+                    ViewBag.TotalPages = (int)Math.Ceiling((double)totalRecords / 15);
+                }
+            }
+
+            ViewBag.CurrentPage = pageNumber;
+            return View(customerList);
+        }
 
         [HttpGet]
         public IActionResult NewCustomer()
@@ -85,7 +118,7 @@ namespace Eco_CRM_Api_Consume_FrontEnd.Controllers
                 return View(request);
             }
         }
-        
+
     }
 
 }
